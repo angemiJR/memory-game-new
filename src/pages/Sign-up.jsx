6eba26d7d90047950168f24deaa1c8853142603d
Remedeae <sanna.m.blomdahl@gmail.com>
2024-12-06 22:12:04 +0100

    Revert "login/signup changes added"
    This reverts commit 59c8c2b12155f373f44b1df622180fd0b8f9d26e.

--- src/pages/Sign-up.jsx
+++ src/pages/Sign-up.jsx
@@ -1,33 +1,8 @@
-import { useState } from 'react';
-import '../styles/reset.css';
-import '../styles/Sign-up.css';
 
-function SignUp() {
 
-    const [userInfo, setUserInfo] = useState();
-
-    function addUserInfo () {
-        
-    }
-
+function SignUp () {
     return (
-        <div className="signup__body">
-            <div className='signup__body_marking marking-L'></div>
-            <form action="">
-                <div className="signup__form">
-                    <div>
-                        <label htmlFor="username">USERNAME</label>
-                        <input type="text" id="username" name="username" placeholder='Select a username' />
-                    </div>
-                    <div>
-                        <label htmlFor="password">PASSWORD</label>
-                        <input type="text" id="password" name="password" placeholder='Select a password' />
-                    </div>
-                    <button onClick={addUserInfo}>SIGN UP</button>
-                </div>
-            </form>
-            <div className='signup__body_marking marking-R'></div>
-        </div>
+        <div></div>
     )
 }
 
