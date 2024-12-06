80c6223d62dd81917699cb63a94fc07d2b599497
Remedeae <sanna.m.blomdahl@gmail.com>
2024-12-06 22:58:47 +0100

    login+signup added
    Added login.jsx/.css and aign-up.jsx/.css.... again

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,24 @@
- function Login() {
+import '../styles/reset.css';
+import '../styles/Login.css'
+
+function Login() {
  
     return (
-        <div></div>
+        <div className="login__body">
+        <form action="">
+            <div className="login__form">
+                <div>
+                    <label htmlFor="username">USERNAME</label>
+                    <input type="text" id="username" name="username" placeholder='Enter username here'/>
+                </div>
+                <div>
+                    <label htmlFor="password">PASSWORD</label>
+                    <input type="text" id="password" name="password" placeholder='Enter password here'/>
+                </div>
+                <button>LOG IN</button>
+            </div>
+        </form>
+    </div>
     );
 
  }
