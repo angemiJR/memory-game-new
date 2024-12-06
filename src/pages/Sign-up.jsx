59c8c2b12155f373f44b1df622180fd0b8f9d26e
Remedeae <sanna.m.blomdahl@gmail.com>
2024-12-06 21:25:29 +0100

    login/signup changes added

--- src/pages/Sign-up.jsx
+++ src/pages/Sign-up.jsx
@@ -1,8 +1,33 @@
+import { useState } from 'react';
+import '../styles/reset.css';
+import '../styles/Sign-up.css';
 
+function SignUp() {
 
-function SignUp () {
+    const [userInfo, setUserInfo] = useState();
+
+    function addUserInfo () {
+        
+    }
+
     return (
-        <div></div>
+        <div className="signup__body">
+            <div className='signup__body_marking marking-L'></div>
+            <form action="">
+                <div className="signup__form">
+                    <div>
+                        <label htmlFor="username">USERNAME</label>
+                        <input type="text" id="username" name="username" placeholder='Select a username' />
+                    </div>
+                    <div>
+                        <label htmlFor="password">PASSWORD</label>
+                        <input type="text" id="password" name="password" placeholder='Select a password' />
+                    </div>
+                    <button onClick={addUserInfo}>SIGN UP</button>
+                </div>
+            </form>
+            <div className='signup__body_marking marking-R'></div>
+        </div>
     )
 }
 
